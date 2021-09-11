d4469479069dcb55c9db783ffbd98b5fcebc15e0
김주성 <8804who>
2021-09-12 01:44:55 +0900

    Default Changelist

---
+++ 백준 연습/백준 13305번 주유소.py
@@ -0,0 +1,20 @@
+N = int(input())
+Distance = list(map(int, input().split()))
+OilPrice = list(map(int, input().split()))
+Price = 0
+Oil = 0
+CityNumber = 0
+for i in range(len(OilPrice)):
+    if i != CityNumber:
+        continue
+    GoDistance = 0
+    for j in range(i, len(Distance)):
+        GoDistance += Distance[j]
+        CityNumber += 1
+        if OilPrice[i] > OilPrice[j+1]:
+            break
+    if Oil < GoDistance:
+        Price += (GoDistance - Oil) * OilPrice[i]
+        Oil += GoDistance-Oil
+    Oil -= GoDistance
+print(Price)
