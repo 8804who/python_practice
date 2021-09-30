14c59d9b6801bd24731d9c6d04f3309020eb9cf3
1a2a3a4a5 <8804who@naver.com>
2021-09-30 14:24:25 +0900

    Default Changelist

---
+++ 백준 연습/백준 11866번 요세푸스 문제 0.py
@@ -0,0 +1,11 @@
+import sys
+from collections import deque
+N, K = map(int, sys.stdin.readline().split())
+deq = deque([i for i in range(1, N+1)])
+delete = K
+print("<", end="")
+for i in range(N-1):
+    for j in range(K):
+        deq.append(deq.popleft())
+    print(deq.pop(), end=", ")
+print(deq.pop(), end=">")
