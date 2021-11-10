460cb6b48bf282bf3f80440d8cb9bd5664dfc73d
1a2a3a4a5 <8804who@naver.com>
2021-11-10 15:47:42 +0900

    Default Changelist

--- 백준 연습/백준 2217번 로프.py
+++ 백준 연습/백준 2217번 로프.py
@@ -1,11 +1,11 @@
 import sys
-input = sys.stdin.readline
-N = int(input())
+inp = sys.stdin.readline
+N = int(inp())
 rope = []
 weight = 0
-max_weight=0
+max_weight = 0
 for i in range(N):
-    rope.append(int(input()))
+    rope.append(int(inp()))
 rope.sort(reverse=True)
 for i in range(N):
     weight = rope[i]*(i+1)
---
+++ 백준 연습/백준 2981번 검문.py
@@ -0,0 +1,38 @@
+import sys
+
+
+def gcd(a, b):
+    while b != 0:
+        r = a % b
+        a = b
+        b = r
+    return a
+
+
+n = int(sys.stdin.readline())
+m = []
+diff = []
+result = []
+for i in range(n):
+    m.append(int(sys.stdin.readline()))
+
+m.sort()
+
+for i in range(1, n):
+    diff.append(m[i]-m[i-1])
+
+prev = diff[0]
+for i in range(1, n-1):
+    prev = gcd(prev, diff[i])
+
+for i in range(2, int(prev**(1/2))+1):
+    if prev % i == 0:
+        result.append(i)
+length = len(result)
+for i in range(length-1, -1, -1):
+    if prev / result[i] != result[i]:
+        result.append(int(prev/result[i]))
+result.append(prev)
+
+for i in range(len(result)):
+    print(result[i], end=" ")
