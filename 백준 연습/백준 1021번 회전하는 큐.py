ed3fc2ee7ef8f0ebe6d61ae77c9955d27a7f0946
1a2a3a4a5 <8804who@naver.com>
2021-11-24 18:54:44 +0900

    Default Changelist

---
+++ 백준 연습/백준 1021번 회전하는 큐.py
@@ -0,0 +1,22 @@
+from collections import deque
+N, M = map(int, input().split())
+q = deque()
+for i in range(1, N+1):
+    q.append(i)
+popList = list(map(int, input().split()))
+num = 0
+count = 0
+while num < M:
+    for i in range(len(q)):
+        if q[i] == popList[num]:
+            where = i
+    if where == 0:
+        q.popleft()
+        num += 1
+    elif where < len(q)/2:
+        q.append(q.popleft())
+        count += 1
+    else:
+        q.appendleft(q.pop())
+        count += 1
+print(count, end="")
