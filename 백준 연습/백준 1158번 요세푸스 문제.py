d568e68d903d28e6a739291ba8ded84c5efb1b2b
1a2a3a4a5 <8804who@naver.com>
2021-11-26 19:53:20 +0900

    Default Changelist

--- 백준 연습/백준 1158번 요세푸스 문제.py
+++ 백준 연습/백준 1158번 요세푸스 문제.py
@@ -1,19 +1,18 @@
 import sys
 from collections import deque
+
 N, K = map(int, sys.stdin.readline().split())
 q = deque()
 num = []
 for i in range(N):
-    q.append(i+1)
+    q.append(i + 1)
 count = 0
-while q:
+print("<", end="")
+while len(q) > 1:
     count += 1
     if count == K:
-        num.append(q.popleft())
+        print(q.popleft(), ",", sep="", end=" ")
         count = 0
     else:
         q.append(q.popleft())
-print("<", end="")
-for i in range(N-1):
-    print(num[i], ",", sep="", end=" ")
-print(num[N-1], ">", sep="", end=" ")
+print(q.popleft(), ">", sep="", end=" ")
