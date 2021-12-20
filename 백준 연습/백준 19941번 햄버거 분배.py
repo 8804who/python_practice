7561638f37b5cf8d33d5847f03e8b4f44cfc74b7
1a2a3a4a5 <8804who@naver.com>
2021-12-20 22:01:38 +0900

    Default Changelist

--- 백준 연습/백준 19941번 햄버거 분배.py
+++ 백준 연습/백준 19941번 햄버거 분배.py
@@ -1,13 +1,11 @@
 import sys
 N, K = map(int, sys.stdin.readline().split())
 strInput = list(sys.stdin.readline().rstrip())
-strInput2 = []
-for i in range(N):
-    strInput2.append(strInput[i])
 count = 0
-count2 = 0
+
 if N < K:
     K = N
+
 for i in range(N):
     if strInput[i] == "P":
         for j in range(-K, K+1):
@@ -16,18 +14,5 @@ for i in range(N):
                     count += 1
                     strInput[i+j] = "N"
                     break
-            elif i+j < N:
-                break
 
-for i in range(N-1, -1, -1):
-    if strInput2[i] == "P":
-        for j in range(K, -K-1, -1):
-            if 0 <= i+j < N:
-                if strInput2[i+j] == "H":
-                    count2 += 1
-                    strInput2[i+j] = "N"
-                    break
-            elif i+j < 0:
-                break
-
-print(max(count, count2), end="")
+print(count, end="")
