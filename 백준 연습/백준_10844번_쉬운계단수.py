a91d349ed5ae0475b3eaea2942236ec9c6808cd0
1a2a3a4a5 <8804who@naver.com>
2022-01-20 23:30:27 +0900

    Default Changelist

---
+++ 백준 연습/백준_10844번_쉬운계단수.py
@@ -0,0 +1,10 @@
+n = int(input())
+memo = [[0 for _ in range(10)] for _ in range(n)]
+for i in range(10):
+    memo[0][i] = 1
+for i in range(1, n):
+    memo[i][0] = memo[i-1][1] % 1000000000
+    for j in range(1, 9):
+        memo[i][j] = memo[i-1][j-1] % 1000000000+memo[i-1][j+1] % 1000000000
+    memo[i][9] = memo[i-1][8] % 1000000000
+print(sum(memo[n-1][1:]) % 1000000000, end="")
