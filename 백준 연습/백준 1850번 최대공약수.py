c5c593208985cfaed5195cda97d0891a0d1b742a
김주성 <8804who@naver.com>
2022-01-30 23:36:24 +0900

    python

--- 백준 연습/백준 1850번 최대공약수.py
+++ 백준 연습/백준 1850번 최대공약수.py
@@ -1,11 +1,10 @@
 import sys
 A, B = map(int, sys.stdin.readline().split())
-A, B = max(A, B), min(A,B)
+A, B = max(A, B), min(A, B)
 while True:
     r = A % B
     if r > 0:
         A, B = B, r
     else:
         break
-for i in range(B):
-    print(1, end="")
+print('1'*B, end="")
