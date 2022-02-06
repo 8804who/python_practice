0fe75feb397bab2c6c9240d9bbc7cd083ef6c390
1a2a3a4a5 <8804who@naver.com>
2022-02-06 19:46:48 +0900

    Default Changelist

---
+++ 백준 연습/백준 1918번 후위 표기식.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+par = False
+expression = sys.stdin.readline().strip()
+stack = deque()
+for i in range(len(expression)):
+    if expression[i] == "(":
+        stack.append(expression[i])
+    elif expression[i] == ")":
+        while stack:
+            char = stack.pop()
+            if char == "(":
+                break
+            else:
+                print(char, end="")
+    elif expression[i] == "+" or expression[i] == "-":
+        while stack:
+            char = stack.pop()
+            if char == "(":
+                stack.append("(")
+                break
+            else:
+                print(char, end="")
+        stack.append(expression[i])
+    elif expression[i] == "*" or expression[i] == "/":
+        if stack:
+            if stack[-1] == "*" or stack[-1] == "/":
+                print(stack.pop(), end="")
+        stack.append(expression[i])
+    else:
+        print(expression[i], end="")
+while stack:
+    print(stack.pop(), end="")
