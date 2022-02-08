2f76a3fee10858f1f6c895e3a8cb96b29428c2f2
1a2a3a4a5 <8804who@naver.com>
2022-02-09 00:08:19 +0900

    Default Changelist

--- 백준 연습/백준 11497번 통나무 건너뛰기.py
+++ 백준 연습/백준 11497번 통나무 건너뛰기.py
@@ -4,12 +4,12 @@ for i in range(T):
     N = int(sys.stdin.readline())
     arr = list(map(int, sys.stdin.readline().split()))
     arr.sort()
-    maxDiff = abs(arr[0]-arr[1])
+    maxDiff = arr[1]-arr[0]
     frontPivot = 0
     backPivot = 2
     for j in range(N-1):
-        maxDiff = max(maxDiff, abs(arr[frontPivot]-arr[backPivot]))
+        maxDiff = max(maxDiff, arr[backPivot]-arr[frontPivot])
         frontPivot += 1
-        if backPivot<N-1:
+        if backPivot < N-1:
             backPivot += 1
-    print(maxDiff, end="")
+    print(maxDiff)
