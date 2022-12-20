c5e7d6a5b2b0f8f5e0ca8a74d923273240d0eaa3
1a2a3a4a5 <8804who@naver.com>
2022-12-21 00:13:37 +0900

    파이썬 PS문제 풀이

---
+++ 프로그래머스/프로그래머스 구명보트.py
@@ -0,0 +1,13 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    light = 0
+    heavy = len(people)-1
+    while heavy >= light:
+        answer += 1
+        weight = 0
+        weight += people[heavy]
+        heavy -= 1
+        if weight+people[light] <= limit:
+            light += 1
+    return answer
