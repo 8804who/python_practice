09d24c4d24b164e589ff038f0665b549cbb3def9
1a2a3a4a5 <8804who@naver.com>
2022-12-22 01:04:31 +0900

    파이썬 PS문제 풀이

---
+++ 프로그래머스/프로그래머스 N개의 최소공배수.py
@@ -0,0 +1,19 @@
+def solution(arr):
+    lcm = 1
+
+    for i in range(len(arr)):
+        gcd = getGCD(lcm, arr[i])
+        lcm = (arr[i] // gcd) * (lcm // gcd) * gcd
+
+    return lcm
+
+
+def getGCD(A, B):
+    if A < B:
+        A, B = B, A
+
+    while A % B != 0:
+        temp = A % B
+        A, B = B, temp
+
+    return B
