06c2d1c2e4ee74702edbc04a3c7dbd4d05e25a58
1a2a3a4a5 <8804who@naver.com>
2023-01-12 17:49:59 +0900

    파이썬 PS문제 풀이

--- 프로그래머스/프로그래머스 택배상자.py
+++ 프로그래머스/프로그래머스 택배상자.py
@@ -2,23 +2,20 @@ from collections import deque
 
 
 def solution(order):
-    answer = 0
-    arr=[[i+1, order[i]] for i in range(len(order))]
-    arr.sort(key = lambda x:x[1])
-    stack=deque()
-    count=1
+    answer = 1
+    arr = [[i+1, order[i]] for i in range(len(order))]
+    arr.sort(key=lambda x: x[1])
+    stack = deque()
 
     for i in range(len(order)):
-        if arr[i][0]==count:
-            count+=1
-            answer+=1
+        if arr[i][0] == answer:
+            answer += 1
             while stack:
-                if stack[-1][0]==count:
+                if stack[-1][0] == answer:
                     stack.pop()
-                    count+=1
-                    answer+=1
+                    answer += 1
                 else:
                     break
         else:
             stack.append(arr[i])
-    return answer
+    return answer-1
