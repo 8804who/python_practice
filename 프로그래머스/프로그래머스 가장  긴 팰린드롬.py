486ad1b0ea7467f2583159781f28bcddd92e6ae8
1a2a3a4a5 <8804who@naver.com>
2023-01-12 16:24:32 +0900

    파이썬 PS문제 풀이

---
+++ 프로그래머스/프로그래머스 가장  긴 팰린드롬.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = 0
+
+    for i in range(len(s)):
+        for j in range(i, len(s)+1):
+            if s[i:j]==s[i:j][::-1]:
+                answer=max(answer, len(s[i:j]))
+    return answer
