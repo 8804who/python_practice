6be06bf72237e16983b414f17e94c71e2db54173
1a2a3a4a5 <8804who@naver.com>
2023-01-13 14:43:59 +0900

    파이썬 PS문제 풀이

---
+++ 프로그래머스/프로그래머스 기지국 설치.py
@@ -0,0 +1,16 @@
+import math
+
+
+def solution(n, stations, w):
+    answer = 0
+
+    s = 1
+    for i in stations:
+        if i - w > s:
+            answer += math.ceil((i - w - s) / (w * 2 + 1))
+        s = i + w + 1
+    if n > s:
+        answer += math.ceil((n - s) / (w * 2 + 1))
+    if n == s:
+        answer += 1
+    return answer
