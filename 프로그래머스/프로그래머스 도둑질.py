48f2968f29d744cacd056eddc3f56f096b21592a
1a2a3a4a5 <8804who@naver.com>
2023-01-17 23:20:10 +0900

    파이썬 PS문제 풀이

--- 프로그래머스/프로그래머스 도둑질.py
+++ 프로그래머스/프로그래머스 도둑질.py
@@ -1,29 +1,29 @@
-def solution(sticker):
+def solution(money):
     answer = 0
-    totalSticker = len(sticker)
+    totalmoney = len(money)
 
-    if totalSticker == 1:
-        return sticker[0]
+    if totalmoney == 1:
+        return money[0]
 
-    dp = [0 for _ in range(totalSticker)]
+    dp = [0 for _ in range(totalmoney)]
 
-    for i in range(0, totalSticker - 1):
+    for i in range(0, totalmoney - 1):
         if i == 0:
-            dp[i] = sticker[i]
+            dp[i] = money[i]
         elif i == 1:
-            dp[i] = max(dp[i - 1], sticker[i])
+            dp[i] = max(dp[i - 1], money[i])
         else:
-            dp[i] = max(dp[i - 2] + sticker[i], dp[i - 1])
-    answer = dp[totalSticker - 2]
+            dp[i] = max(dp[i - 2] + money[i], dp[i - 1])
+    answer = dp[totalmoney - 2]
 
-    for i in range(totalSticker):
+    for i in range(totalmoney):
         dp[i] = 0
 
-    for i in range(1, totalSticker):
+    for i in range(1, totalmoney):
         if i == 1:
-            dp[i] = sticker[i]
+            dp[i] = money[i]
         else:
-            dp[i] = max(dp[i - 2] + sticker[i], dp[i - 1])
-    if answer < dp[totalSticker - 1]:
-        answer = dp[totalSticker - 1]
+            dp[i] = max(dp[i - 2] + money[i], dp[i - 1])
+    if answer < dp[totalmoney - 1]:
+        answer = dp[totalmoney - 1]
     return answer
