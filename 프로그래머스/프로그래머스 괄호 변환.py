c316c839937a8f21b0e325d801da35a90ec0723f
1a2a3a4a5 <8804who@naver.com>
2023-01-19 15:58:22 +0900

    파이썬 PS문제 풀이

---
+++ 프로그래머스/프로그래머스 괄호 변환.py
@@ -0,0 +1,52 @@
+from collections import deque
+
+
+def solution(p):
+    return change(p)
+
+
+def change(w):
+    count = 0
+    u = ''
+    for i in range(len(w)):
+        if w[i] == '(':
+            count += 1
+        else:
+            count -= 1
+        if count == 0:
+            u += w[0:i + 1]
+            break
+    v = w[len(u):]
+    correct = True
+
+    for i in u:
+        if i == '(':
+            count += 1
+        else:
+            count -= 1
+        if count == -1:
+            correct = False
+            break
+
+    if len(v) > 0:
+        if correct:
+            return u + change(v)
+        else:
+            temp = ''
+            for c in u:
+                if c == '(':
+                    temp += ')'
+                else:
+                    temp += '('
+            return '(' + change(v) + ')' + temp[1:-1]
+    else:
+        if correct:
+            return u
+        else:
+            temp = ''
+            for c in u:
+                if c == '(':
+                    temp += ')'
+                else:
+                    temp += '('
+            return '(' + ')' + temp[1:-1]
