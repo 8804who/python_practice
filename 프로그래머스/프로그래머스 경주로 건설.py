f1835b751e672aae6b46b5a330a3c6773f9dccdd
1a2a3a4a5 <8804who@naver.com>
2023-06-19 17:03:55 +0900

    파이썬 PS문제 풀이

--- 프로그래머스/프로그래머스 경주로 건설.py
+++ 프로그래머스/프로그래머스 경주로 건설.py
@@ -18,9 +18,11 @@ def solution(board):
             moved_x, moved_y = x + move[i][0], y + move[i][1]
             if length > moved_x >= 0 and length > moved_y >= 0 and board[moved_x][moved_y] == 0:
                 if d == i:
-                    if cost[moved_x][moved_y] + 600 >= c + 100:
+                    if cost[moved_x][moved_y] >= c + 100:
                         q.append([moved_x, moved_y, i, c + 100])
                         cost[moved_x][moved_y] = c + 100
+                    elif cost[moved_x][moved_y] + 600 >= c + 100:
+                        q.append([moved_x, moved_y, i, c + 100])
                 else:
                     if cost[moved_x][moved_y] >= c + 600:
                         q.append([moved_x, moved_y, i, c + 600])
