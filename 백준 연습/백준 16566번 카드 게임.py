c3802962316e85e55c695a50d93b704d21f61af3
1a2a3a4a5 <8804who@naver.com>
2023-06-20 10:44:51 +0900

    파이썬 PS문제 풀이

---
+++ 백준 연습/백준 16566번 카드 게임.py
@@ -0,0 +1,34 @@
+import sys
+from bisect import bisect_right
+
+
+def union(a, b):
+    A = find(a)
+    B = find(b)
+
+    if A >= B:
+        parent[B] = A
+    else:
+        parent[A] = B
+
+
+def find(a):
+    if a != parent[a]:
+        parent[a] = find(parent[a])
+    return parent[a]
+
+
+N, M, K = map(int, sys.stdin.readline().split())
+
+parent = [i for i in range(M)]
+
+cards = list(map(int, sys.stdin.readline().rstrip().split()))
+cs_cards = list(map(int, sys.stdin.readline().rstrip().split()))
+
+cards.sort()
+
+for cs_card in cs_cards:
+    num = bisect_right(cards, cs_card)
+    print(cards[find(num)])
+    if find(num)+1 < M:
+        union(find(num), find(num)+1)
