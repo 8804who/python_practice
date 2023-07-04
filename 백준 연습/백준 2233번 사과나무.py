a2b4bedf49de538c0335d454a6d59decae51a1f7
1a2a3a4a5 <8804who@naver.com>
2023-07-04 14:20:58 +0900

    파이썬 PS문제 풀이

---
+++ 백준 연습/백준 2233번 사과나무.py
@@ -0,0 +1,55 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(int(1e5))
+
+
+def make_tree(node, d):
+    depth[node] = d
+    for c in graph[node]:
+        if depth[c] != -1:
+            continue
+        parent[c] = node
+        make_tree(c, d+1)
+
+
+def lca(a, b):
+    while depth[a] != depth[b]:
+        if depth[a] > depth[b]:
+            a = parent[a]
+        else:
+            b = parent[b]
+    while a != b:
+        a = parent[a]
+        b = parent[b]
+    return a
+
+
+N = int(sys.stdin.readline())
+parent = [0 for _ in range(N+1)]
+graph = [[] for _ in range(N+1)]
+depth = [-1 for _ in range(N+1)]
+
+stack = deque()
+log = []
+node = 0
+
+for char in sys.stdin.readline()[:-2]:
+    if char == '0':
+        node += 1
+        stack.append(node)
+        log.append(node)
+    else:
+        n = stack.pop()
+        log.append(n)
+        graph[stack[-1]].append(n)
+
+root = stack.pop()
+log.append(root)
+make_tree(root, 0)
+
+a,b = map(int, sys.stdin.readline().split())
+cut_point = lca(log[a-1], log[b-1])
+
+for idx, node in enumerate(log):
+    if node == cut_point:
+        print(idx+1, end = ' ')
