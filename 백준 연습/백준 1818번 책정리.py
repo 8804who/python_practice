409fadeed16b4d716d662cc46085eb39bf177337
1a2a3a4a5 <8804who@naver.com>
2023-07-19 23:09:54 +0900

    파이썬 PS문제 풀이

---
+++ 백준 연습/백준 1818번 책정리.py
@@ -0,0 +1,12 @@
+import sys
+from bisect import bisect_left as bs
+
+N = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().split()))
+lis = [nums[0]]
+for num in nums[1:]:
+    if num>lis[-1]:
+        lis.append(num)
+    else:
+        lis[bs(lis, num)] = num
+print(N-len(lis))
