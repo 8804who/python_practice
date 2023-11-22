669d4a28a50404387f7621e2b897a81c934cf398
1a2a3a4a5 <8804who@naver.com>
2023-11-22 20:53:44 +0900

    파이썬 PS문제 풀이

---
+++ 백준 연습/백준 3015번 오아시스 재결합.py
@@ -0,0 +1,23 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+heights = [int(sys.stdin.readline()) for _ in range(N)]
+stack = [[heights.pop(), 1]]
+answer = 0
+
+for height in heights[::-1]:
+    while stack and stack[-1][0] < height:
+        answer += stack[-1][1]
+        stack.pop()
+
+    if stack and stack[-1][0] == height:
+        answer += stack[-1][1]
+        if len(stack) > 1:
+            answer += 1
+        stack[-1][1] += 1
+    else:
+        if stack:
+            answer += 1
+        stack.append([height, 1])
+print(answer)
