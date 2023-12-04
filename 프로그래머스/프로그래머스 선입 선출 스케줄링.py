6a666398f29f8a70c7d4e2a5ed37ea78d662a7d4
1a2a3a4a5 <8804who@naver.com>
2023-12-04 21:58:50 +0900

    파이썬 PS문제 풀이

---
+++ 프로그래머스/프로그래머스 선입 선출 스케줄링.py
@@ -0,0 +1,30 @@
+def solution(n, cores):
+    cores = [0] + cores
+
+    time = 1e10
+    start = 1
+    end = 500000000
+    while start <= end:
+        mid = (start + end) // 2
+        work = n
+        for core in cores[1:]:
+            work -= mid // core
+            if mid % core != 0:
+                work -= 1
+        if work <= 0:
+            end = mid - 1
+            if time > end:
+                time = end
+        else:
+            start = mid + 1
+
+    for core in cores[1:]:
+        n -= time // core
+        if time % core != 0:
+            n -= 1
+
+    for i in range(1, len(cores)):
+        if time % cores[i] == 0:
+            n -= 1
+            if n == 0:
+                return i
