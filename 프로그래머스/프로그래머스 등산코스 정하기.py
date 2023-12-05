54c8ac03060a52c8792633a27e6ec0009ef0a533
1a2a3a4a5 <8804who@naver.com>
2023-12-05 17:00:43 +0900

    파이썬 PS문제 풀이

--- 프로그래머스/프로그래머스 등산코스 정하기.py
+++ 프로그래머스/프로그래머스 등산코스 정하기.py
@@ -5,7 +5,7 @@ def solution(n, paths, gates, summits):
     answer = [1e9, 1e9]
     summits.sort()
     graph = [[] for _ in range(n + 1)]
-    is_gate_or_summit = [False] * (n + 1)
+    is_summit = [False] * (n + 1)
 
     for path in paths:
         graph[path[0]].append([path[1], path[2]])
@@ -18,16 +18,15 @@ def solution(n, paths, gates, summits):
         nodes[gate] = 0
         for route in graph[gate]:
             heapq.heappush(heap, [route[1], route[0]])
-        is_gate_or_summit[gate] = True
 
     for summit in summits:
-        is_gate_or_summit[summit] = True
+        is_summit[summit] = True
 
     while heap:
         intensity, node = heapq.heappop(heap)
         if nodes[node] > intensity:
             nodes[node] = intensity
-            if not is_gate_or_summit[node]:
+            if not is_summit[node]:
                 for route in graph[node]:
                     heapq.heappush(heap, [max(intensity, route[1]), route[0]])
 
