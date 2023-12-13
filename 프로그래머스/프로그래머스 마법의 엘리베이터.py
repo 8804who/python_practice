54a551264d6953f1d5c84af8741d460bee8f8bf0
1a2a3a4a5 <8804who@naver.com>
2023-12-13 15:46:11 +0900

    파이썬 PS문제 풀이

--- 프로그래머스/프로그래머스 마법의 엘리베이터.py
+++ 프로그래머스/프로그래머스 마법의 엘리베이터.py
@@ -9,7 +9,7 @@ def solution(storey):
             temp_storey = storey - n * div
             if temp_storey == 0:
                 answer += n
-                storey -= n * (div)
+                storey -= n * div
             else:
                 temp_div = div * 10
                 while temp_storey > 0:
@@ -18,22 +18,22 @@ def solution(storey):
                         temp_storey -= temp_n
                         if temp_storey == 0:
                             answer += n
-                            storey += n * (div)
+                            storey += n * div
                         temp_div *= 10
 
                     elif temp_n > 5 * temp_div:
                         answer += n
-                        storey += n * (div)
+                        storey += n * div
                         break
                     else:
                         answer += n
-                        storey -= n * (div)
+                        storey -= n * div
                         break
         elif n > 5:
             answer += 10 - n
-            storey += (10 - n) * (div)
+            storey += (10 - n) * div
         else:
             answer += n
-            storey -= n * (div)
+            storey -= n * div
         div *= 10
     return answer
