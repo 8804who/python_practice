2f86e04992a7e10c35c56e86daf4aa8678d36ea4
1a2a3a4a5 <8804who@naver.com>
2024-01-10 15:03:33 +0900

    파이썬 PS문제 풀이

--- 못 푼 문제/백준 7578번 공장.py
+++ 못 푼 문제/백준 7578번 공장.py
@@ -3,54 +3,34 @@ import sys
 input = sys.stdin.readline
 
 
-def makeMaxTree(left, right, n):
-    if left == right:
-        max_tree[n] = arr[left]
-        return max_tree[n]
-
-    mid = (left+right)//2
-    left_min = makeMaxTree(left, mid, n*2)
-    right_min = makeMaxTree(mid+1, right, n*2+1)
-    max_tree[n] = max(left_min, right_min)
-    return max_tree[n]
-
-
-def makeMinTree(left, right, n):
-    if left == right:
-        min_tree[n] = arr[left]
-        return min_tree[n]
-
-    mid = (left+right)//2
-    left_min = makeMinTree(left, mid, n*2)
-    right_min = makeMinTree(mid+1, right, n*2+1)
-    min_tree[n] = min(left_min, right_min)
-    return min_tree[n]
-
-
-def Query(start, end, n, left, right, bm):
-    global answer
+def Query(start, end, n, left, right):
     if end < left or start > right:
-        return
+        return 0
     if start <= left and right <= end:
-        if max_tree[n] < bm:
-            answer += right-left+1
-            return
-        if min_tree[n] > bm:
-            return
+        return segment_tree[n]
     mid = (left+right)//2
-    Query(start, end, 2*n, left, mid, bm)
-    Query(start, end, 2*n+1, mid+1, right, bm)
+    left_sum = Query(start, end, 2*n, left, mid)
+    right_sum = Query(start, end, 2*n+1, mid+1, right)
+    return left_sum + right_sum
+
+
+def change(start, end, idx, n):
+    if idx < start or idx > end:
+        return 0
+    segment_tree[n] += 1
+    if start != end:
+        mid = (start+end)//2
+        change(start, mid, idx, n*2)
+        change(mid+1, end, idx, n*2+1)
 
 
 N = int(input())
 dic = {v: i for i, v in enumerate(input().split())}
 arr = tuple(dic[i] for i in input().split())
-max_tree = [0]*(N*4)
-min_tree = [0]*(N*4)
+segment_tree = [0]*(N*4)
 answer = 0
-makeMaxTree(0, N-1, 1)
-makeMinTree(0, N-1, 1)
 
-for i in range(N-1):
-    Query(i+1, N-1, 1, 0, N-1, arr[i])
+for i in arr:
+    answer += Query(i+1, N-1, 1, 0, N-1)
+    change(0, N-1, i, 1)
 print(answer)
