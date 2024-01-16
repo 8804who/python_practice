b82430cd167767ed50705acd5bbb62d7cbe481a9
1a2a3a4a5 <8804who@naver.com>
2024-01-16 23:46:13 +0900

    파이썬 PS문제 풀이

---
+++ 프로그래머스/프로그래머스 연속 펄스 부분 수열의 합.py
@@ -0,0 +1,20 @@
+def solution(sequence):
+    answer = 0
+    sequence1 = [sequence[i] * (1 if i % 2 == 0 else -1) for i in range(len(sequence))]
+    sequence2 = [sequence[i] * (-1 if i % 2 == 0 else 1) for i in range(len(sequence))]
+
+    n1, n2 = 0, 0
+    for num1, num2 in zip(sequence1, sequence2):
+        n1 += num1
+        n2 += num2
+        if n1 < 0:
+            n1 = 0
+        if n2 < 0:
+            n2 = 0
+
+        if answer < n1:
+            answer = n1
+        if answer < n2:
+            answer = n2
+
+    return answer
