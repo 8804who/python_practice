265b8b6cf2569c61d1add4c017332c1493bdfd4f
1a2a3a4a5 <8804who@naver.com>
2024-01-19 16:20:54 +0900

    파이썬 PS문제 풀이

--- 프로그래머스/프로그래머스 기둥과 보 설치.py
+++ 프로그래머스/프로그래머스 기둥과 보 설치.py
@@ -1,7 +1,6 @@
 def solution(n, build_frame):
     answer = []
-    pillar_frame = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
-    beam_frame = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
+    frame = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
     pillar_fixture = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
     beam_fixture = [[0 for _ in range(n + 2)] for _ in range(n + 2)]
 
@@ -12,43 +11,43 @@ def solution(n, build_frame):
         x, y, a, b = build
         if b == 0:
             if a == 0:
-                if pillar_frame[y + 1][x] == 1 and pillar_fixture[y + 1][x] == 1:
+                if frame[y + 1][x] & (1 << 0) and pillar_fixture[y + 1][x] == 1:
                     continue
-                if x - 1 >= 0 and beam_frame[y + 1][x - 1] == 1 and beam_fixture[y + 1][x - 1] < 4:
+                if x - 1 >= 0 and frame[y + 1][x - 1] & (1 << 1) and beam_fixture[y + 1][x - 1] < 4:
                     continue
-                if beam_frame[y + 1][x] == 1 and beam_fixture[y + 1][x] < 4:
+                if frame[y + 1][x] & (1 << 1) and beam_fixture[y + 1][x] < 4:
                     continue
                 pillar_fixture[y + 1][x] -= 1
                 beam_fixture[y + 1][x] -= 2
                 if x - 1 >= 0:
                     beam_fixture[y + 1][x - 1] -= 2
-                pillar_frame[y][x] = 0
+                frame[y][x] ^= 1 << 0
             else:
-                if x - 1 >= 0 and beam_frame[y][x - 1] == 1 and beam_fixture[y][x - 1] < 3:
+                if x - 1 >= 0 and frame[y][x - 1] & (1 << 1) and beam_fixture[y][x - 1] < 3:
                     continue
-                if beam_frame[y][x + 1] == 1 and beam_fixture[y][x + 1] < 3:
+                if frame[y][x + 1] & (1 << 1) and beam_fixture[y][x + 1] < 3:
                     continue
-                if pillar_frame[y][x + 1] == 1 and pillar_fixture[y][x + 1] == 1:
+                if frame[y][x + 1] & (1 << 0) and pillar_fixture[y][x + 1] == 1:
                     continue
-                if pillar_frame[y][x] == 1 and pillar_fixture[y][x] == 1:
+                if frame[y][x] & (1 << 0) and pillar_fixture[y][x] == 1:
                     continue
                 if x - 1 >= 0:
                     beam_fixture[y][x - 1] -= 1
                 beam_fixture[y][x + 1] -= 1
                 pillar_fixture[y][x + 1] -= 1
                 pillar_fixture[y][x] -= 1
-                beam_frame[y][x] = 0
+                frame[y][x] ^= 1 << 1
         else:
             if a == 0:
                 if pillar_fixture[y][x] > 0:
-                    pillar_frame[y][x] = 1
+                    frame[y][x] |= 1 << 0
                     pillar_fixture[y + 1][x] += 1
                     if x - 1 >= 0:
                         beam_fixture[y + 1][x - 1] += 2
                     beam_fixture[y + 1][x] += 2
             else:
                 if beam_fixture[y][x] > 1:
-                    beam_frame[y][x] = 1
+                    frame[y][x] |= 1 << 1
                     if x - 1 >= 0:
                         beam_fixture[y][x - 1] += 1
                     beam_fixture[y][x + 1] += 1
@@ -57,8 +56,7 @@ def solution(n, build_frame):
 
     for x in range(n + 1):
         for y in range(n + 1):
-            if pillar_frame[y][x] == 1:
-                answer.append([x, y, 0])
-            if beam_frame[y][x] == 1:
-                answer.append([x, y, 1])
+            for i in range(2):
+                if frame[y][x] & (1 << i):
+                    answer.append([x, y, i])
     return answer
