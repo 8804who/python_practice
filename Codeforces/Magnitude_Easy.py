f92406b411a82e3b786c2e1a9cb1f7d28722f0ed
1a2a3a4a5 <8804who@naver.com>
2024-06-10 01:06:50 +0900

    파이썬 PS문제 풀이

---
+++ Codeforces/Magnitude_Easy.py
@@ -0,0 +1,19 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for testcase in range(t):
+    n = int(sys.stdin.readline())
+    arr = list(map(int, sys.stdin.readline().split()))
+
+    min_n = 0
+    max_n = 0
+
+    for i in range(n):
+        min_n += arr[i]
+        max_n += arr[i]
+        if max_n < 0:
+            max_n *= -1
+        if -min_n > max_n:
+            max_n = -min_n
+    print(max_n)
