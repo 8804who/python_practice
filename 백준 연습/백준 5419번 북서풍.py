6d79cb7c3d0c7a131460686fa6e42e49b19a662a
1a2a3a4a5 <8804who@naver.com>
2024-11-14 23:09:47 +0900

    백준 5419번 북서풍

--- 백준 연습/백준 5419번 북서풍.py
+++ 백준 연습/백준 5419번 북서풍.py
@@ -55,7 +55,6 @@ for _ in range(T):
     islands.sort(key=lambda x:(x[1], x[0]))
     answer = 0
     for i in range(n):
-
         answer += query(0, n-1, 0, islands[i][0]-1, 1)
         change(0, n-1, islands[i][0]-1, 1)
     print(answer)
