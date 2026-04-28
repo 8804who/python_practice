0e365761c21427dd7741aef098e16f3a0b40c6ab
8804 <8804who@naver.com>
2026-04-28 23:27:23 +0900

    LeetCode 77 Combinations

---
+++ LeetCode/LeetCode 77 Combinations.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        answer = []
+        for c in combinations(range(1,n+1), k):
+            answer.append(c)
+        return answer
